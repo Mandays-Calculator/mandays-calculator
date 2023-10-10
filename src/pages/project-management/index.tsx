d714e9faa12c680b6a343c45ad7d051be70eccdf
riejan <eguitariejan@gmail.com>
2023-10-10 16:05:40 +0800

    Controlled Textfield

--- src/pages/project-management/index.tsx
+++ src/pages/project-management/index.tsx
@@ -1,9 +1,12 @@
-import type { ReactElement } from 'react'
+import type { ReactElement } from "react";
+import AddProject from "./add-project";
 
 const ProjectManagement = (): ReactElement => {
   return (
-    <div>Project Management</div>
-  )
-}
+    <div>
+      <AddProject />
+    </div>
+  );
+};
 
-export default ProjectManagement
+export default ProjectManagement;
