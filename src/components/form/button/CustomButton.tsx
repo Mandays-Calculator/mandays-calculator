19319a88b646359ffad3ca23f5d55fed9c64761f
george-dev <georgesoriano74@gmail.com>
2023-10-11 20:54:53 +0800

    feat: add custom button

---
+++ src/components/form/button/CustomButton.tsx
@@ -0,0 +1,61 @@
+import React from "react";
+import Button, { ButtonProps } from "@mui/material/Button";
+import { styled } from "@mui/material/styles";
+
+type ColorVariants =
+  | "primary"
+  | "primaryDark"
+  | "primaryLight"
+  | "secondary"
+  | "error"
+  | "warning"
+  | "success";
+
+interface StyledButtonProps extends ButtonProps {
+  colorVariant: ColorVariants;
+}
+
+const StyledButton = styled(Button)<StyledButtonProps>(
+  ({ theme, colorVariant }) => {
+    const colorsMap: Record<ColorVariants, string> = {
+      primary: theme.palette.primary.main,
+      primaryDark: theme.palette.primary.dark,
+      primaryLight: theme.palette.primary.light,
+      secondary: theme.palette.secondary.main,
+      error: theme.palette.error.main,
+      warning: theme.palette.warning.main,
+      success: theme.palette.success.main,
+    };
+    const contrastTextMap: Record<ColorVariants, string> = {
+      primary: theme.palette.primary.contrastText,
+      primaryDark: theme.palette.primary.contrastText,
+      primaryLight: "#000000",
+      secondary: theme.palette.primary.contrastText,
+      error: theme.palette.primary.contrastText,
+      warning: theme.palette.primary.contrastText,
+      success: theme.palette.primary.contrastText,
+    };
+
+    return {
+      backgroundColor: colorsMap[colorVariant],
+      color: contrastTextMap[colorVariant],
+      textTransform: "none",
+      "&:hover": {
+        backgroundColor: colorsMap[colorVariant], // Keep the same background color on hover
+        opacity: 0.9,
+      },
+    };
+  }
+);
+interface CustomButtonProps extends ButtonProps {
+  colorVariant?: ColorVariants;
+}
+
+export const CustomButton: React.FC<CustomButtonProps> = ({
+  colorVariant = "primary",
+  ...props
+}) => {
+  return <StyledButton colorVariant={colorVariant} {...props} />;
+};
+
+export default CustomButton;
