ec3946850fc30cd5c0f0eae016918601bc6db513
george.dev <gsoriano.ext@gmail.com>
2023-10-17 23:30:20 +0800

    feat: add odc landing page, add padding and border radius in custom button

--- src/pages/odc-management/ODCManagement.tsx
+++ src/pages/odc-management/ODCManagement.tsx
@@ -1,7 +1,29 @@
 import type { ReactElement } from "react";
+import { Grid, TextField, styled } from "@mui/material";
+import { PageContainer } from "~/components/page-container";
+import Title from "~/components/title/Title";
+import { CustomButton } from "~/components/form/button";
+
+const StyledTextField = styled(TextField)(() => ({
+  width: "100%",
+}));
 
 const ODCManagement = (): ReactElement => {
-  return <div>Test</div>;
+  return (
+    <>
+      <Title title="ODC Management" />
+      <PageContainer sx={{ background: "#FFFFFF" }}>
+        <Grid container spacing={2} alignItems="center">
+          <Grid item xs={5}>
+            <StyledTextField size="small" placeholder="Enter keyword here..." />
+          </Grid>
+          <Grid item xs={7} container justifyContent="flex-end">
+            <CustomButton type="button">Add ODC</CustomButton>
+          </Grid>
+        </Grid>
+      </PageContainer>
+    </>
+  );
 };
 
 export default ODCManagement;
