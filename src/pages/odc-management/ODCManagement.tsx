2dc522104479307baf45d1d06d1db473329a2a66
82009755 <82009755@corp.root>
2023-10-18 11:00:43 +0800

    feat: add table

--- src/pages/odc-management/ODCManagement.tsx
+++ src/pages/odc-management/ODCManagement.tsx
@@ -1,27 +1,12 @@
 import type { ReactElement } from "react";
-import { Grid, TextField, styled } from "@mui/material";
-import { PageContainer } from "~/components/page-container";
 import Title from "~/components/title/Title";
-import { CustomButton } from "~/components/form/button";
-
-const StyledTextField = styled(TextField)(() => ({
-  width: "100%",
-}));
+import ViewODC from "./view-list/ViewODC";
 
 const ODCManagement = (): ReactElement => {
   return (
     <>
       <Title title="ODC Management" />
-      <PageContainer sx={{ background: "#FFFFFF" }}>
-        <Grid container spacing={2} alignItems="center">
-          <Grid item xs={5}>
-            <StyledTextField size="small" placeholder="Enter keyword here..." />
-          </Grid>
-          <Grid item xs={7} container justifyContent="flex-end">
-            <CustomButton type="button">Add ODC</CustomButton>
-          </Grid>
-        </Grid>
-      </PageContainer>
+      <ViewODC />
     </>
   );
 };
