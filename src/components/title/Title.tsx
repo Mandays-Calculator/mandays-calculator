e78cd9b65103f650354ff675c746acf51fba146f
82008562 <82008562@corp.root>
2023-10-20 13:36:23 +0800

    Fix: title fontsize

--- src/components/title/Title.tsx
+++ src/components/title/Title.tsx
@@ -1,21 +1,22 @@
 import type { ReactElement } from "react";
-
+import { styled } from "@mui/material/styles";
 import Typography from "@mui/material/Typography";
 
 interface TitleProps {
   title: string;
   color?: string;
 }
+
+const StyledTitle = styled(Typography)`
+  font-size: 2.5rem;
+`;
+
 const Title = (props: TitleProps): ReactElement => {
   const { title, color = "primary" } = props;
   return (
-    <Typography
-      color={color}
-      fontSize={40}
-      fontWeight="600"
-    >
+    <StyledTitle color={color} fontWeight="600">
       {title}
-    </Typography>
+    </StyledTitle>
   );
 };
 
