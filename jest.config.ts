1e59d6ce6cdab9bbed192573bde892a7cd0f06da
george.dev <gsoriano.ext@gmail.com>
2023-10-21 14:42:47 +0800

    feat: add coverage/ in gitignore, add test for custom button and modify jest config

--- jest.config.ts
+++ jest.config.ts
@@ -4,29 +4,29 @@ const config: Config = {
   preset: "ts-jest",
   testEnvironment: "jest-environment-jsdom",
   setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
-
+  modulePaths: ["<rootDir>"],
+  moduleFileExtensions: ["js", "jsx", "ts", "tsx", "js", "node"],
+  moduleDirectories: ["node_modules", "src"],
   transform: {
     "^.+\\.tsx?$": "ts-jest",
   },
   moduleNameMapper: {
     "\\.css$": "identity-obj-proxy",
+    "^~/(.*)": "<rootDir>/src/$1",
+    "@mui/system/(.*)": "<rootDir>/node_modules/@mui/system/$1",
   },
-  //   collectCoverage: true,
-  //   collectCoverageFrom: ["**/*.{js,jsx}", "!**/node_modules/**"],
-  //   coverageReporters: ["clover", "json", "lcov", ["text", { skipFull: true }]],
-  //   coverageThreshold: {
-  //     global: {
-  //       branches: 80,
-  //       functions: 80,
-  //       lines: 80,
-  //       statements: -10,
-  //     },
-  //   },
-  globals: {
-    "ts-jest": {
-      tsconfig: "./tsconfig.json",
-    },
-  },
+  forceCoverageMatch: ["**/*.test.(ts|tsx|js|jsx)"],
+  collectCoverage: true,
+  coverageReporters: ["text", "lcov"],
+  coverageDirectory: "<rootDir>/coverage",
+  collectCoverageFrom: [
+    "**/*.(ts|tsx)",
+    "!**/node_modules/**",
+    "!**/coverage/**",
+    "!**/*json",
+    "!**/*.d.ts",
+    "!vite.config.ts",
+  ],
 };
 
 export default config;
