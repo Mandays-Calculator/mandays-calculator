897dd1c24407f1d1b92b68eb1034fadfa4f883ee
82008562 <82008562@corp.root>
2023-11-07 16:48:23 +0800

    Fix: remove console

--- src/api/axios.config.ts
+++ src/api/axios.config.ts
@@ -1,7 +1,6 @@
 import axios, { InternalAxiosRequestConfig, AxiosError } from "axios";
 
 const init = async (kcToken: string | undefined): Promise<void> => {
-  console.log(kcToken);
   axios.interceptors.request.use(
     (config: InternalAxiosRequestConfig) => {
       config.headers.Authorization = `Bearer ${kcToken}`;
