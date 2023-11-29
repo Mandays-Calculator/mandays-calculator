ed9e645dcb593a7889425f0683f8deb949ed1303
82008562 <82008562@corp.root>
2023-11-29 20:54:05 +0800

    Fix: restore dashboard default

--- src/pages/dashboard/Dashboard.tsx
+++ src/pages/dashboard/Dashboard.tsx
@@ -1,25 +1,5 @@
-import Steppers from "~/components/stepper/Stepper";
-
 function Dashboard() {
-  return (
-    <div>
-      <Steppers
-        activeStep={0}
-        steps={[
-          {
-            label: "Summary",
-            icon: "mandays_estimation_tool",
-            content: <>Miss u</>,
-          },
-          {
-            label: "Resource",
-            icon: "mandays_estimation_tool",
-            content: <>Test</>,
-          },
-        ]}
-      />
-    </div>
-  );
+  return <div>Dashboard</div>;
 }
 
 export default Dashboard;
