431e53328d41e4d8cbce6832757d1970e77075cb
Mditalo <mditalo.ext@gmail.com>
2024-02-02 16:08:27 +0800

    feat: mandays estimation modal mock up alignment

--- src/pages/mandays-calculator/estimation-details/components/share-modal/generated-link/GeneratedLink.tsx
+++ src/pages/mandays-calculator/estimation-details/components/share-modal/generated-link/GeneratedLink.tsx
@@ -1,13 +1,14 @@
-import type { ReactElement } from "react"
+import type { ReactElement } from "react";
 import type { TFunction } from "i18next";
 
 import Grid from "@mui/material/Grid";
 import Typography from "@mui/material/Typography";
-import Button from "@mui/material/Button";
 
 import { ControlledTextField } from "~/components/form/controlled";
-import { CustomButton } from "~/components/form/button";
 import LocalizationKey from "~/i18n/key";
+
+import { StyledBackButton, StyledLinkButton } from "../styles";
+import { Box, Stack } from "@mui/material";
 
 type RenderLinkProps = {
   link: string;
@@ -17,38 +18,45 @@ type RenderLinkProps = {
 
 const GeneratedLink = (props: RenderLinkProps): ReactElement => {
   const { link, setIsShare, t } = props;
-  const { mandaysCalculator: { modal } } = LocalizationKey;
+  const {
+    mandaysCalculator: { modal },
+  } = LocalizationKey;
 
   return (
-    <Grid container sx={{ mt: 4 }}>
+    <Grid mt={3}>
       <Grid item xs={12}>
-        <Typography variant="body1" fontWeight="bold">
-          {t(modal.link)}:
-        </Typography>
+        <Typography variant="body1" fontWeight="bold"></Typography>
         <ControlledTextField name="link" disabled value={link} />
       </Grid>
-      <Grid container justifyContent="s" sx={{ mt: 1 }} rowGap={1}>
-        <Grid item xs={3}>
-          <Button variant="outlined" onClick={() => setIsShare(false)}>Back</Button>
-        </Grid>
-        <Grid item xs={9}>
-          <Grid container justifyContent={"end"} rowGap={2} columnGap={2}>
-            <Grid item>
-              <Button 
-                variant="outlined" 
-                onClick={() => window.open(`${window.location.origin}/mandays-estimation-detail`, '_blank', 'noreferrer')}
-              >
-                {t(modal.redirect)}
-              </Button>
-            </Grid>
-            <Grid item>
-              <CustomButton>{t(modal.copyLink)}</CustomButton>
-            </Grid>
-          </Grid>
-        </Grid>
+
+      <Grid item xs={12} my={5}>
+        <Stack flexDirection="row" justifyContent="flex-end">
+          <Box pr={2}>
+            <StyledBackButton
+              variant="text"
+              color="inherit"
+              onClick={() => setIsShare(false)}
+            >
+              Back
+            </StyledBackButton>
+          </Box>
+          <StyledLinkButton
+            variant="contained"
+            color="primary"
+            onClick={() =>
+              window.open(
+                `${window.location.origin}/mandays-estimation-detail`,
+                "_blank",
+                "noreferrer",
+              )
+            }
+          >
+            {t(modal.redirect)}
+          </StyledLinkButton>
+        </Stack>
       </Grid>
     </Grid>
   );
 };
 
-export default GeneratedLink; 
+export default GeneratedLink;
